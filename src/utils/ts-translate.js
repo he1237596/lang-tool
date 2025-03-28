19638e7d84612d24fe71eca80db7d44c390956ca
Chris <he1237596@163.com>
2025-03-28 14:35:02 +0800

    feat: 更新下文件

--- src/utils/ts-translate.js
+++ src/utils/ts-translate.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 // 到smipor用
 const langDir = path.resolve( __dirname,'./src/lang'); // 替换为你的 lang 目录路径
-const targetDir = path.resolve( __dirname,'./src/locales'); // 替换为你的 lang 目录路径
+const targetDir = path.resolve( __dirname,'./src/lang/locales'); // 替换为你的 lang 目录路径
 const fileNames = []
 async function main() {
     const files = fs.readdirSync(langDir);
@@ -16,7 +16,11 @@ async function main() {
             const translations = require(filePath).default;
 
             // 写入对应的 JSON 文件
-            const jsonFilePath = path.join(targetDir, `translations/${path.basename(file, '.ts')}.json`);
+            const jsonFilePath = path.join(targetDir, `${path.basename(file, '.ts')}.json`);
+            // 创建文件
+            // if (!fs.existsSync(jsonFilePath)) {
+            //     fs.writeFileSync(jsonFilePath, '{}');
+            // }
             fs.writeFileSync(jsonFilePath, JSON.stringify(translations, null, 2));
 
             console.log(`已写入: ${jsonFilePath}`);
