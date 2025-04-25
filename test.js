2ce92c47c7f05fe07f4952252847fca8e3b0fbd9
Chris <he1237596@163.com>
2025-04-25 09:30:53 +0800

    test: 测试

--- test.js
+++ test.js
@@ -18,11 +18,11 @@ const createTranslationKeys = async (batchData) => {
     //     });
     //     console.log(`翻译键 ${keyName} 已成功创建`, response.data);
     // }
-    const res = await axios.post(`http://127.0.0.1:8888/api/projects/10/keys/bulk_create`, {
+    const res = await axios.post(`http://127.0.0.1:8888/api/projects/1/keys/bulk_create`, {
       data: batchData
     }, {
       headers: {
-        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOiI1ZWI4YjNhOC0wZTUwLTQ5ZjUtYmM0NC1jNzhkYjQ5Mzg5OGUiLCJpYXQiOjE3NDMwNTk2MDUsImV4cCI6MTc0MzA2NjgwNX0.EHJBJlopFtAJovEKAYneeX-_ESVtdlkt-gRAOadsoxM'
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOiIwYWE0MTU4NC0xYzQ3LTQ2NDQtYTBkNC1iZjIwMWRlMTQzNjMiLCJpYXQiOjE3NDQyNzM5MzgsImV4cCI6MTc0NDI4MTEzOH0.DsVehJmCpLJM9H5aSY47d5M18Eur7Eu-kTV-WTckyRE'
       }
     });
     console.log(`成功创建`, res.data);
